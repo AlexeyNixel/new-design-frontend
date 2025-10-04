dc2ab290935e0bb4ea3f81edd143094ea5fab663
A. Pechenkin <pechekin685@gmail.com>
2025-10-04 18:28:30 +0700

    Переработана шапка сайта, улучшен внешний вид

---
+++ app/components/ui/HeaderPop.vue
@@ -0,0 +1,36 @@
+<template>
+  <UPopover mode="hover">
+    <div
+      class="grid grid-cols-[auto_minmax(120px,_180px)] gap-2 p-1 items-center justify-center rounded-xl transition hover:bg-primary hover:text-white hover:cursor-pointer"
+    >
+      <Icon :name="icon" :class="`text-${size}`" />
+      <div>
+        <span class="">{{ label }}</span>
+      </div>
+    </div>
+
+    <template #content v-if="isPopover">
+      <div class="rounded-xl overflow-hidden">
+        <component :is="modal" />
+      </div>
+    </template>
+  </UPopover>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    icon: string;
+    label: string;
+    size?: '5xl' | '2xl';
+    isPopover?: boolean;
+    modal?: Component;
+  }>(),
+  {
+    size: '5xl',
+    isPopover: true,
+  }
+);
+</script>
+
+<style scoped></style>
